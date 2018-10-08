316668ba7737ed41bda8855c1315e1e10248e2ac
Rohith uppala <rohithuppala28@gmail.com>
2018-10-08 12:40:09 -0400

    Table creation and update

---
+++ src/connectorHelper.py
@@ -0,0 +1,20 @@
+import psycopg2
+import os
+
+class LocalConnector:
+    def __init__(self, psql=True):
+        self.conn = None
+        # Connect to Redshift
+        host_red = os.environ["REDSHIFT_HOST"]
+        dbname_redshift = os.environ["database_name"]
+        user_redshift = os.environ["redshift_user"]
+        password_redshift = os.environ["redshift_pwd"]
+        port_redshift = 5439
+        self.conn = psycopg2.connect(host=host_red, dbname=dbname_redshift,
+                                user=user_redshift, password=password_redshift, port=port_redshift)
+
+    def get_connection(self):
+        return self.conn
+
+    def close_connection(self):
+        self.conn.close()
--- src/table_Creation.py
+++ src/table_Creation.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+y  # !/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Created on Tue Sep 25 16:50:19 2018
@@ -9,83 +9,72 @@ Created on Tue Sep 25 16:50:19 2018
 import pymysql
 import time
 import sys
-class movies():
-    # Initialize
-    def __init__(self,clusterURL = "local",appName = "AUTH"):
-        
-        self.userName = os.environ["user"]
-        self.password = os.environ["pwd"]
-        
-        # Debug statement
-        print("Initialized")
+from DatabaseConnector import DatabaseConnector
 
-    def connect_database(self):
-        try:
-            host="mysqlinstance.cjm8qag6rwgx.us-east-1.rds.amazonaws.com"
-            port=3306
-            dbname="moviesdatabase"
-        
-            conn = pymysql.connect(host, user=self.userName,port=port,
-                           passwd=self.password, db=dbname)
-            self.conn = conn
-        except:
-            print("cannot connect to db")
-        
-        return conn
-    
-    def create_cursor(self,conn):
+
+class movies():
+
+    def create_cursor(self, conn):
         cur = conn.cursor()
         print("connection esatblished")
         return cur
 
-    def create_tables(self,curObj):
-        
-        
-        print("Data ingestion started ")
+    def create_tables(self):
+
+        dbconnector = DatabaseConnector()
+        connection = dbconnector.get_connection()
+        curObj = connection.cursor()
+        cnt =0
+        print("Creating Tables ")
         start_time = time.time()
-        cnt=0
         try:
-            # Movies table created 
-            sqlQuery = "CREATE TABLE IF NOT EXISTS movies (movie_id INT AUTO_INCREMENT PRIMARY KEY, movie_name VARCHAR(255)  NOT NULL, runtime INT,budget INT, revenue BIGINT, language VARCHAR(255), popularity FLOAT,releasedate DATE, votes INT, voteAvg INT)";
+            # Movies table created
+            sqlQuery = "CREATE TABLE IF NOT EXISTS movies (movie_id INT AUTO_INCREMENT PRIMARY KEY, " \
+                       "movie_name VARCHAR(255)  NOT NULL, runtime INT,budget INT, revenue BIGINT, language VARCHAR(255), " \
+                       "popularity FLOAT,releasedate DATE, votes INT, voteAvg INT)";
             curObj.execute(sqlQuery)
             print("Movies table created")
-            
-            
+
             # Actor tables created
-            actorQuery = "CREATE TABLE actors (actorId INT AUTO_INCREMENT PRIMARY KEY, movie_id int NOT NULL, actorName VARCHAR(255),characterName VARCHAR(255), gender INT, FOREIGN KEY(movie_id) REFERENCES movies(movie_id));"
+            actorQuery = "CREATE TABLE actors (actorId INT AUTO_INCREMENT PRIMARY KEY, " \
+                         "movie_id int NOT NULL, actorName VARCHAR(255),characterName VARCHAR(255), " \
+                         "gender INT, FOREIGN KEY(movie_id) REFERENCES movies(movie_id));"
             curObj.execute(actorQuery)
             print("Actors table created")
-            
+
             # Generes tables created
-            genresQuery = "CREATE TABLE genres (genreid INT AUTO_INCREMENT PRIMARY KEY, movie_id int NOT NULL, genreName VARCHAR(255), FOREIGN KEY(movie_id) REFERENCES movies(movie_id));"
+            genresQuery = "CREATE TABLE genres (genreid INT AUTO_INCREMENT PRIMARY KEY, " \
+                          "movie_id int NOT NULL, genreName VARCHAR(255), FOREIGN KEY(movie_id) " \
+                          "REFERENCES movies(movie_id));"
             curObj.execute(genresQuery)
             print("Genres table created")
-            
+
             # Crew tables created
-            crewQuery = "CREATE TABLE crew (crewId INT AUTO_INCREMENT PRIMARY KEY, movie_id int NOT NULL, crewName VARCHAR(255),dept VARCHAR(255), FOREIGN KEY(movie_id) REFERENCES movies(movie_id));"
+            crewQuery = "CREATE TABLE crew (crewId INT AUTO_INCREMENT PRIMARY KEY, " \
+                        "movie_id int NOT NULL, crewName VARCHAR(255),dept VARCHAR(255), " \
+                        "FOREIGN KEY(movie_id) REFERENCES movies(movie_id));"
             curObj.execute(crewQuery)
             print("Crew table created")
-            
+
             # Production tables created
-            productionQuery = "CREATE TABLE production (pId INT AUTO_INCREMENT PRIMARY KEY, movie_id int NOT NULL, pName VARCHAR(255), FOREIGN KEY(movie_id) REFERENCES movies(movie_id));"
+            productionQuery = "CREATE TABLE production (pId INT AUTO_INCREMENT PRIMARY KEY, " \
+                              "movie_id int NOT NULL, pName VARCHAR(255), " \
+                              "FOREIGN KEY(movie_id) REFERENCES movies(movie_id));"
             curObj.execute(productionQuery)
             print("Production table created")
-        
+
         except pymysql.err.ProgrammingError as err:
-             print(err)
+            print(err)
         except:
             print("Error in the creating tables")
             print("Unexpected error:", sys.exc_info()[0])
-        
-        print("Time taken to execute %s"%(time.time() - start_time))                
-        print("problem with %s",cnt)
+        curObj.close()
+        connection.commit()
+        dbConnector.close_connection()
+        print("Time taken to execute %s" % (time.time() - start_time))
+        print("problem with %s", cnt)
+
 
 if __name__ == "__main__":
-    
     movieInstance = movies()
-    conn = movieInstance.connect_database()
-    cur= movieInstance.create_cursor(conn)
-    movieInstance.create_tables(cur)
-    conn.commit()
-    conn.close()
-    
+    movieInstance.create_tables()
